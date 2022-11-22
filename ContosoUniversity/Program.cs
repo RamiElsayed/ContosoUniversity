fcdbc3392e32dbe8b3d8d6bc1220f0cda4211313
Rami Elsayed <ramielsayed123@outlook.com>
2022-11-22 16:13:22 +0000

    added services and seeded data

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -1,7 +1,15 @@
+using ContosoUniversity.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<SchoolContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
